abd23235fc07fe4d4e265e50ea6e19a92f5fe9b4
DiegoAbenicio <diego.abenicio1@gmail.com>
2021-08-09 10:44:20 -0300

    links

--- js/setup.js
+++ js/setup.js
@@ -15,5 +15,37 @@ $('.owl-carousel').owlCarousel({
     }
 })
  function chamar(){
-    window.location.assign("../music.html");
+    window.location.assign("music.html");
+}
+function animes(valor){
+    var link;
+    if(valor == 1){
+        link = "https://www.youtube.com/watch?v=GwaRztMaoY0&ab_channel=CrunchyrollCollection"
+    } else if(valor == 2){
+        link = "https://www.youtube.com/watch?v=lTlzDfhPtFA"
+    } else if(valor == 3){
+        link = "https://www.youtube.com/watch?v=y7333GgriD8"
+    } else if(valor == 4){
+        link = "https://www.youtube.com/watch?v=pmanD_s7G3U"
+    } else if(valor == 5){
+        link = "https://www.youtube.com/watch?v=Ku5fgOHy1JY"
+    } else if(valor == 6){
+        link = "https://www.youtube.com/watch?v=VtfUrDwpo2E"
+    } else if(valor == 7){
+        link = "https://www.youtube.com/watch?v=Bw-5Lka7gPE"
+    } else if(valor == 8){
+        link = "https://www.youtube.com/watch?v=QuHdSHMS4bk&ab_channel=AnimeSekai"
+    } else if(valor == 9){
+        link = "https://www.youtube.com/watch?v=KOWcj7XKnfQ"
+    } else if(valor == 10){
+        link = "https://www.youtube.com/watch?v=By_JYrhx-WY"
+    }
+    window.open(link);
+}
+function musicas(valor){
+    var link;
+    if(valor == 1){
+        link = "https://www.youtube.com/watch?v=zRToTk5b620&ab_channel=ZelpexKappa"
+    }
+    window.open(link);
 }
