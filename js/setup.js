6c2385f8960dad501980acff0cea5dc053622cac
DiegoAbenicio <diego.abenicio1@gmail.com>
2021-08-09 20:48:03 -0300

    Funcional

--- js/setup.js
+++ js/setup.js
@@ -44,8 +44,44 @@ function animes(valor){
 }
 function musicas(valor){
     var link;
-    if(valor == 1){
-        link = "https://www.youtube.com/watch?v=zRToTk5b620&ab_channel=ZelpexKappa"
+    switch(valor){
+        case 1:
+            link = "https://www.youtube.com/watch?v=zRToTk5b620&ab_channel=ZelpexKappa"
+        break;
+        case 2:
+            link = "https://www.youtube.com/watch?v=tIhL2KHVdgE&ab_channel=KADOKAWAanime"
+        break;
+        case 3:
+            link = "https://www.youtube.com/watch?v=S_BWr4mUwy4&ab_channel=Tsubomi"
+        break;
+        case 4:
+            link = "https://www.youtube.com/watch?v=CXcEbr1ixEc&ab_channel=AniMelody–AnimeMusic"
+        break;
+        case 5:
+            link = "https://www.youtube.com/watch?v=wp7l9oolXS0&ab_channel=AniMelody–AnimeMusic"
+        break;
+        case 6:
+            link = "https://www.youtube.com/watch?v=AwHapidVQlI&ab_channel=AniMelody–AnimeMusic"
+        break;
+        case 7:
+            link = "https://www.youtube.com/watch?v=SQulCltPdRY&ab_channel=yuuChannネル"
+        break;
+        case 8:
+            link = "https://www.youtube.com/watch?v=fV3Fu5csdcA&ab_channel=HDMelody"
+        break;
+        case 9:
+            link = "https://www.youtube.com/watch?v=5c_jTxvutlw&ab_channel=MelodicStar"
+        break;
+        case 10:
+            link = "https://www.youtube.com/watch?v=Yc8pu8TC7IM&ab_channel=KADOKAWAanime"
+        break;
+        case 11:
+            link = "https://www.youtube.com/watch?v=MnytTcodsPk&ab_channel=MelodicStar"
+        break;
+        default:
+            link = "https://www.youtube.com/watch?v=AYjLTtsl-hY&ab_channel=InfinityTone"
+        break;
+                   
     }
     window.open(link);
 }
